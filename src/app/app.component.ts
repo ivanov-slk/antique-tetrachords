ec883f53bc74e63f80b1bbf1553eca9423999da6
Slav <ivanov.slk@abv.bg>
2019-11-11 10:13:22 +0200

    added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Tone from 'tone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'antique-tetrachords';
+  synth: any;
+
+  ngOnInit() {}
+
+  onTest() {
+    //create a synth and connect it to the master output (your speakers)
+    this.synth = new Tone.Synth().toMaster();
+
+    this.synth.triggerAttackRelease(440, 2);
+    this.synth.triggerAttackRelease(500, '2n', '+3');
+  }
 }
