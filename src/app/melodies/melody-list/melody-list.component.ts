514bff0e9fd4d37c5d71790af147dde7b1a8aa36
Slav <ivanov.slk@abv.bg>
2019-11-23 17:46:59 +0200

    added Byzantine section

--- src/app/melodies/melody-list/melody-list.component.ts
+++ src/app/melodies/melody-list/melody-list.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { MelodyService } from '../melody.service';
-import { Melody } from '../melody.model';
+import { Component, OnInit, Input } from "@angular/core";
+import { MelodyService } from "../melody.service";
+import { Melody } from "../melody.model";
 
 @Component({
-  selector: 'app-melody-list',
-  templateUrl: './melody-list.component.html',
-  styleUrls: ['./melody-list.component.css']
+  selector: "app-melody-list",
+  templateUrl: "./melody-list.component.html",
+  styleUrls: ["./melody-list.component.css"]
 })
 export class MelodyListComponent implements OnInit {
   melodies: Melody[];
+  melodyGroup: string;
 
   constructor(private melodyService: MelodyService) {}
 
   ngOnInit() {
-    this.melodies = this.melodyService.getMelodies();
+    this.melodyGroup = "antique";
+    this.melodies = this.melodyService.getMelodies(this.melodyGroup);
+  }
+
+  setMelodyGroup(value: string) {
+    this.melodyGroup = value;
+    console.log(this.melodyGroup);
+    this.melodies = this.melodyService.getMelodies(value);
   }
 }
